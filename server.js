1df55bd7b8506a52bf76c167a9087ec9f450ea22
Kairos Jeong <kairosys@gmail.com>
2022-04-27 23:47:33 -0700

    first commit in Office

---
+++ server.js
@@ -0,0 +1,24 @@
+//++기본문법
+const express = require("express");
+const app = express();
+//----
+
+app.listen(8080, function () {
+  console.log("listening on 8080");
+});
+
+app.get("/pet", function (요청, 응답) {
+  응답.send("pet 쇼핑입니다.");
+});
+
+app.get("/beauty", function (요청, 응답) {
+  응답.send("Beauty용품 쇼핑입니다.");
+});
+
+app.get("/", function (요청, 응답) {
+  응답.sendFile(__dirname + "/index.html");
+});
+
+app.get("/write", function (요청, 응답) {
+  응답.sendFile(__dirname + "/write.html");
+});
